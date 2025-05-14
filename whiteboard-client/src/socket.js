bb9bae8f99150efc216019456ba8cb6aef89c1bc
Satyam1000BECSE <128610133+Satyam1000BECSE@users.noreply.github.com>
2025-05-14 16:02:29 +0530

    Update socket.js

--- whiteboard-client/src/socket.js
+++ whiteboard-client/src/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://real-time-whiteboards-server-project.vercel.app');
+const socket = io('https://real-time-whiteboards-server-project.onrender.com');
 export default socket;
